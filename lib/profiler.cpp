14cf1e92e1929687bd367a24906d3e2abb78e34b
Raoul Wols <raoulwols@gmail.com>
2015-11-14 11:00:01 +0100

    Modified profiler class and driver info tool.
    The profiler class lives in a namespace called
    gintonic::detail now. Also using the __PRETTY_FUNCTION__
    macro so that we don't have to supply the name of the
    function anymore.
    
    The driver info tool now only prints the renderer
    name and version by default. If given the command line argument
    "--extensions", or "-e", the tool will also print all the
    OpenGL extensions of the renderer.

--- lib/profiler.cpp
+++ lib/profiler.cpp
@@ -3,13 +3,21 @@
 #include <thread>
 
 namespace gintonic {
+namespace detail {
 
-___profiler___::~___profiler___()
+profiler::~profiler() noexcept
 {
-	const auto t2 = std::chrono::high_resolution_clock::now();
-	s_log[m_scope_name].push_back(t2 - m_t1);
+	try
+	{
+		const auto t2 = std::chrono::high_resolution_clock::now();
+		s_log[m_scope_name].push_front(t2 - m_t1);
+	}
+	catch (...)
+	{
+		// do nothing
+	}
 }
-void ___profiler___::write_log(const char* log_file)
+void profiler::write_log(const char* log_file)
 {
 	using std::chrono::duration_cast;
 	using std::chrono::nanoseconds;
@@ -21,16 +29,22 @@ void ___profiler___::write_log(const char* log_file)
 	{
 		if (p.second.empty()) continue;
 		average = nanoseconds::zero();
-		for (const auto& d : p.second) average += d;
-		average /= p.second.size();
+		std::size_t size = 0;
+		for (const auto& d : p.second)
+		{
+			average += d;
+			++size;
+		}
+		average /= size;
 		output 
-			<< '\"' << p.first << "\"," << p.second.size() 
+			<< '\"' << p.first << "\"," << size 
 			<< "," << average.count() << '\n';
 	}
 }
 
 std::map<std::string, std::list<std::chrono::high_resolution_clock::duration>> 
-___profiler___::s_log = 
+profiler::s_log = 
 std::map<std::string, std::list<std::chrono::high_resolution_clock::duration>>();
 
+} // end of namespace detail
 } // end of namespace gintonic
