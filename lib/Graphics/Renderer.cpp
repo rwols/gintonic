f8f39643bb05668b0eed3bdfc3fa32f9b8919136
Raoul Wols <raoulwols@gmail.com>
2016-12-23 12:29:13 +0100

    Modified cmake lists files a lot.

--- lib/Graphics/Renderer.cpp
+++ lib/Graphics/Renderer.cpp
@@ -23,7 +23,13 @@
 #include "EntityVisitor.hpp"
 
 #include <iostream>
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #include <SDL.h>
+#pragma clang diagnostic pop
+#endif // __clang__
 
 #ifdef BOOST_MSVC
 	#include <Objbase.h> // for CoInitializeEx
--- lib/cmake/modules/FindFBX.cmake
+++ lib/cmake/modules/FindFBX.cmake
@@ -1,157 +1,155 @@
+################################################################################
 # Locate FBX
 # This module defines:
 #
-# FBX_INCLUDE_DIR -- The FBX SDK include directory
-# FBX_LIBRARY -- The FBX SDK library (debug or release, depending on CMake's configuration)
-# FBX_NAMESPACE -- The C++ name of the FBX SDK namespace. This is useful for forward-declaring FBX types.
-# FBX_DLL -- The DLL location. Copy this into the build directory if necessary.
-# FBX_FOUND -- Wether everything went okay.
+# FBX_INCLUDE_DIRS -- The FBX SDK include directories.
+# FBX_LIBARIES     -- The FBX SDK libraries. Depending on the build type, the
+#                     debug or release version is chosen. If BUILD_SHARED_LIBS
+#                     is ON, then this variable will contain the shared library
+#                     variants. Otherwise, it will contain the static library
+#                     variants.
+# FBX_FOUND        -- Wether everything went okay.
 #
-# If CMake has trouble finding the FBX library, you can define
-# and environment variable called FBX_ROOT that should point tro
-# the root folder of the FBX SDK. For example, if the FBX SDK is
-# located at C:\FBX\2016.0, then FBX_ROOT should resolve to that folder.
+# If CMake has trouble finding the FBX library, you can define an environment 
+# variable called FBX_ROOT that should point to the root folder of the FBX SDK.
+# For example, if the FBX SDK is located at C:\FBX\2016.0, then FBX_ROOT should 
+# resolve to C:\FBX\2016.0. It is also possible to specify the FBX_ROOT variable
+# as a CMake cache variable.
 #
-# If you want to use a specific FBX version, you define a variable
-# named FBX_PREFER_VERSION. For example, set(FBX_PREFER_VERSION "2015.1")
+# If you want to use a specific FBX version, you can define a variable named
+# FBX_PREFER_VERSION. For example, set(FBX_PREFER_VERSION 2015.1 CACHE INTERNAL)
+################################################################################
 
-# Prepend more version numbers when they get released
-# REMARK: PREPEND, not APPEND.
 if (FBX_PREFER_VERSION)
     set(versions ${FBX_PREFER_VERSION})
 else ()
-    set (versions "2016.1.2" "2016.0" "2015.1" "2014.2" "2014.1")
+    # Prepend more version numbers when they get released
+    set(versions
+        2017.0
+        2016.1.2
+        2016.1
+        2016
+        2015.1
+        2015
+        2014.2.1
+        2014.1
+        2013.3
+        2012.2
+        2011.3.1
+        2011.3
+        2011.2
+        2010.2
+        2010.0.2
+        2009.3
+        2009.1
+        2006.11.1
+        2005.12.1
+        2005.12
+        )
 endif ()
 
-set(GCC_OR_CLANG OFF)
-
-set(CLANG_COMPILER OFF)
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    set(CLANG_COMPILER ON)
-endif ()
-
-if (CMAKE_COMPILER_IS_GNUCXX OR CLANG_COMPILER)
-    set(GCC_OR_CLANG ON)
-endif ()
-
-if (APPLE)
-    # set (libdir "gcc4/ub")
-    set (libdir "clang")
-elseif (GCC_OR_CLANG)
-    set (libdir "gcc4")
-elseif (MSVC80)
-    set (libdir "vs2005") 
-elseif (MSVC90)
-    set (libdir "vs2008")
-elseif (MSVC10)
-    set (libdir "vs2010")
-elseif (MSVC11)
-    set (libdir "vs2012")
-elseif (MSVC_VERSION EQUAL 1800)
-    set (libdir "vs2013")
-elseif (MSVC_VERSION EQUAL 1900)
-    set (libdir "vs2015")
-else ()
-    message(FATAL_ERROR "Failed to find FBX: Could not determine compiler version.")
-endif ()
-
-if (APPLE)
-    # do nothing
-elseif (CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set (libdir "${libdir}/x64")
-else ()
-    set (libdir "${libdir}/x86")
-endif ()
-
-set (search_path_prefixes 
-    "$ENV{ProgramW6432}/Autodesk/FBX/FBX SDK"
-    "$ENV{programfiles}/Autodesk/FBX/FBX SDK"
-    "/Applications/Autodesk/FBX SDK")
-
-foreach (search_path IN LISTS search_path_prefixes)
-    foreach (version IN LISTS versions)
-        list(APPEND search_paths "${search_path}/${version}")
-    endforeach()
-endforeach ()
-
-set (fbx_libname "fbxsdk")
-
-if (WIN32)
-    set (fbx_libname "lib${fbx_libname}.lib")
-    if (BUILD_SHARED_LIBS)
-        add_definitions(/D FBXSDK_SHARED)
-    endif()
-else()
-    if (BUILD_SHARED_LIBS)
-        # do nothing
-    else()
-        set(fbx_libname "lib${fbx_libname}.a")
-    endif()
-endif()
-
-list(APPEND search_paths "$ENV{FBX_ROOT}")
-
-find_path(FBX_INCLUDE_DIR "fbxsdk.h"
-    PATHS ${search_paths}
-    PATH_SUFFIXES "include")
-
-find_library(FBX_LIBRARY ${fbx_libname}
-    PATHS ${search_paths}
-    PATH_SUFFIXES "lib/${libdir}/release")
-
-find_library(FBX_LIBRARY_DEBUG ${fbx_libname}
-    PATHS ${search_paths}
-    PATH_SUFFIXES "lib/${libdir}/debug")
-
-# First-time run
-if (FBX_INCLUDE_DIR AND NOT FBX_FOUND)
-    if (WIN32 OR APPLE)
-    foreach (version IN LISTS versions)
-        string(REGEX MATCH ${version} FBX_NAMESPACE ${FBX_INCLUDE_DIR})
-        if (FBX_NAMESPACE)
-            string(REPLACE "." "_" FBX_NAMESPACE ${FBX_NAMESPACE})
-            set(FBX_NAMESPACE "fbxsdk_${FBX_NAMESPACE}" CACHE STRING "The Fbx namespace.")
-            message(STATUS "FBX namespace: ${FBX_NAMESPACE}")
-            break()
-        endif ()
-    endforeach ()
+if (WIN32) # FIXME!
+    set(compiler_id vs2005 vs2008 vs2010 vs2012 vs2013 vs2015)
+    if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(arch x64)
     else ()
-        # BIG HACK!!! IMPROVE THIS
-        # set(FBX_NAMESPACE "fbxsdk_2015_1" CACHE STRING "BIG HACK!!!")
-    endif ()
-endif ()
-
-IF(FBX_LIBRARY AND FBX_LIBRARY_DEBUG AND FBX_INCLUDE_DIR)
-    set (FBX_FOUND ON CACHE BOOL "Fbx has been found.")
-else ()
-    set (FBX_FOUND OFF CACHE BOOL "Fbx has not been found.")
-endif ()
-
-set(FBX_LIBRARY optimized ${FBX_LIBRARY} debug ${FBX_LIBRARY_DEBUG} CACHE STRING "Fbx Library location.")
-
-if (CMAKE_BUILD_TYPE)
-    if (${CMAKE_BUILD_TYPE} STREQUAL Debug)
-        set (FBX_DLL "${FBX_LIBRARY_DEBUG}")
-    else ()
-        set (FBX_DLL "${FBX_LIBRARY}")
+        set(arch x86)
     endif()
-else()
-    set (FBX_DLL "${FBX_LIBRARY_DEBUG}")
-endif()
-
-string(REPLACE "\\" "\\\\" FBX_DLL "${FBX_DLL}")
-
-if (WIN32)
-    string(REPLACE ".lib" ".dll" FBX_DLL "${FBX_DLL}")
+    set (search_path_prefixes 
+        "$ENV{ProgramW6432}/Autodesk/FBX/FBX\ SDK"
+        "$ENV{programfiles}/Autodesk/FBX/FBX\ SDK"
+        )
+    set(libdir_suffix lib/${compiler_id}/${arch}/release)
+    set(libdir_suffix_debug lib/${compiler_id}/${arch}/debug)
+    set(fbx_libname libfbxsdk.lib)
+elseif (APPLE)
+    set(compiler_id clang)
+    set(search_path_prefixes "/Applications/Autodesk/FBX\ SDK")
+    if (BUILD_SHARED_LIBS)
+        set(fbx_libname libfbxsdk.dylib)
+    else (BUILD_SHARED_LIBS)
+        set(fbx_libname libfbxsdk.a)
+    endif (BUILD_SHARED_LIBS)
+    set(libdir_suffix lib/${compiler_id})
+    if (FBX_USE_LIBCXX)
+        set(libdir_suffix ${libdir_suffix}/libstdcpp)
+    endif (FBX_USE_LIBCXX)
+    set(libdir_suffix_debug ${libdir_suffix}/debug)
+    set(libdir_suffix ${libdir_suffix}/release)
+elseif (UNIX)
+    set(compiler_id gcc4)
+    if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(arch x64)
+    else ()
+        set(arch x86)
+    endif()
+    set(search_path_prefixes /usr/local)
+    set(libdir_suffix lib/${compiler_id}/${arch}/release)
+    set(libdir_suffix_debug lib/${compiler_id}/${arch}/debug)
+    if (BUILD_SHARED_LIBS)
+        set(fbx_libname libfbxsdk.so)
+    else (BUILD_SHARED_LIBS)
+        set(fbx_libname libfbxsdk.a)
+    endif (BUILD_SHARED_LIBS)
+else (WIN32)
+    message(FATAL_ERROR "Could not determine platform!")
 endif (WIN32)
 
+list(APPEND search_paths "${FBX_ROOT}")
+list(APPEND search_paths "$ENV{FBX_ROOT}")
+
+foreach (search_path_prefix ${search_path_prefixes})
+    foreach (version ${versions})
+        list(APPEND search_paths "${search_path_prefix}/${version}")
+    endforeach (version)
+endforeach (search_path_prefix)
+
+find_path(FBX_INCLUDE_DIRS 
+    NAMES fbxsdk.h
+    PATHS ${search_paths}
+    PATH_SUFFIXES include
+    NO_DEFAULT_PATH)
+
+find_library(FBX_LIBRARY 
+    NAMES ${fbx_libname}
+    PATHS ${search_paths}
+    PATH_SUFFIXES ${libdir_suffix}
+    NO_DEFAULT_PATH)
+
+find_library(FBX_LIBRARY_DEBUG 
+    NAMES ${fbx_libname}
+    PATHS ${search_paths}
+    PATH_SUFFIXES ${libdir_suffix_debug}
+    NO_DEFAULT_PATH)
+
+set(FBX_LIBRARIES optimized ${FBX_LIBRARY} debug ${FBX_LIBRARY_DEBUG})
+
+if (IS_DIRECTORY "${FBX_INCLUDE_DIRS}")
+    file(READ "${FBX_INCLUDE_DIRS}/fbxsdk/fbxsdk_version.h" fbx_version_file)
+    string(REGEX MATCH "#define FBXSDK_VERSION_MAJOR\t[0-9]+" 
+        fbxver_major "${fbx_version_file}")
+    string(REGEX REPLACE "#define FBXSDK_VERSION_MAJOR\t([0-9]+)" "\\1" 
+        fbxver_major "${fbxver_major}")
+    string(REGEX MATCH "#define FBXSDK_VERSION_MINOR\t[0-9]+" 
+        fbxver_minor "${fbx_version_file}")
+    string(REGEX REPLACE "#define FBXSDK_VERSION_MINOR\t([0-9]+)" "\\1" 
+        fbxver_minor "${fbxver_minor}")
+    string(REGEX MATCH "#define FBXSDK_VERSION_POINT\t[0-9]+" 
+        fbxver_point "${fbx_version_file}")
+    string(REGEX REPLACE "#define FBXSDK_VERSION_POINT\t([0-9]+)" "\\1" 
+        fbxver_point "${fbxver_point}")
+else ()
+    message(SEND_ERROR "FBX include directory not found!")
+endif ()
+
+if (DEFINED fbxver_major AND DEFINED fbxver_minor AND DEFINED fbxver_point)
+    set(FBX_VERSION ${fbxver_major}.${fbxver_minor}.${fbxver_point})
+else ()
+    message(SEND_ERROR "Could not determine FBX version!")
+endif ()
+
 include(FindPackageHandleStandardArgs)
-
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(FBX REQUIRED_VARS FBX_LIBRARY FBX_INCLUDE_DIR)
-
-mark_as_advanced(FBX_FOUND)
-mark_as_advanced(FBX_INCLUDE_DIR)
-mark_as_advanced(FBX_LIBRARY)
-mark_as_advanced(FBX_LIBRARY_DEBUG)
-mark_as_advanced(FBX_DLL)
-mark_as_advanced(FBX_NAMESPACE)
+find_package_handle_standard_args(FBX 
+    FOUND_VAR FBX_FOUND 
+    REQUIRED_VARS FBX_LIBRARIES FBX_INCLUDE_DIRS 
+    VERSION_VAR FBX_VERSION)
