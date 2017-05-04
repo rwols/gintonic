fafa7ec0a4c5b893087e0ff9488ee178487b7c7a
Raoul Wols <raoulwols@gmail.com>
2017-05-04 17:57:52 +0200

    Remove warnings on MSVC

--- lib/Asset.cpp
+++ lib/Asset.cpp
@@ -6,57 +6,53 @@ using namespace gintonic;
 
 std::string Asset::sAssetFolder = std::string();
 
-std::unordered_map<std::string, std::weak_ptr<Asset>> sAssetMap 
-	= std::unordered_map<std::string, std::weak_ptr<Asset>>();
+std::unordered_map<std::string, std::weak_ptr<Asset>> sAssetMap =
+    std::unordered_map<std::string, std::weak_ptr<Asset>>();
 
-Asset::Asset(const std::string& relativeFilename) 
-: mName(relativeFilename)
+Asset::Asset(const std::string& relativeFilename) : mName(relativeFilename)
 {
-	GT_PROFILE_FUNCTION;
+    GT_PROFILE_FUNCTION;
 }
 
 Asset::Asset(std::string&& relativeFilename)
-: mName(std::move(relativeFilename))
+    : mName(std::move(relativeFilename))
 {
-	GT_PROFILE_FUNCTION;
+    GT_PROFILE_FUNCTION;
 }
 
-Asset::Asset(const char* relativeFilename)
-: mName(relativeFilename)
+Asset::Asset(const char* relativeFilename) : mName(relativeFilename)
 {
-	GT_PROFILE_FUNCTION;
+    GT_PROFILE_FUNCTION;
 }
 
 std::string Asset::getFullPathFor(const char* relativeFilename)
 {
-	using boost::filesystem::path;
-	const auto fullPath = path(getAssetFolder()) / path(relativeFilename);
-	return fullPath.string();
+    using boost::filesystem::path;
+    const auto fullPath = path(getAssetFolder()) / path(relativeFilename);
+    return fullPath.string();
 }
 
 std::string Asset::getFullPathFor(const std::string& relativeFilename)
 {
-	return getFullPathFor(relativeFilename.c_str());
+    return getFullPathFor(relativeFilename.c_str());
 }
 
 std::string Asset::getFullPathFor(std::string&& relativeFilename)
 {
-	using boost::filesystem::path;
-	const auto fullPath = path(getAssetFolder()) / path(std::move(relativeFilename));
-	return fullPath.string();
+    using boost::filesystem::path;
+    const auto fullPath =
+        path(getAssetFolder()) / path(std::move(relativeFilename));
+    return fullPath.string();
 }
 
-std::string Asset::getFullPath() const
-{
-	return getFullPathFor(mName);
-}
+std::string Asset::getFullPath() const { return getFullPathFor(mName); }
 
 std::ifstream Asset::openForReading() const
 {
-	return std::ifstream(getFullPath(), std::ios::binary);
+    return std::ifstream(getFullPath(), std::ios::binary);
 }
 
 std::ofstream Asset::openForWriting() const
 {
-	return std::ofstream(getFullPath(), std::ios::binary);
+    return std::ofstream(getFullPath(), std::ios::binary);
 }
